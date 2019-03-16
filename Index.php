a5fb02b208613fdf159912ee4034c3c07f077f60
gia bao le <38273241+giabao125463@users.noreply.github.com>
2019-03-16 14:17:16 +0700

    dua file Index.php vao du an

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php include_once("header.php"); ?>
+<ul class="menu">
+	<li>
+		<a href="/lab4/list_product.php"> danh sach san pham</a>
+	</li>
+	<li>
+		<a href="/lab4/add_product.php">them san pham</a>
+	</li>
+</ul>
+<?php include_once("footer.php"); ?>
