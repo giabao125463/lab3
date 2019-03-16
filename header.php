ba0e46c28c6c1433d21e9d97bff03f522d38e4da
gia bao le <38273241+giabao125463@users.noreply.github.com>
2019-03-16 14:16:05 +0700

    dua vao file header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="legiabao">
+	<link rel="stylesheet" type="text/css" href="/lab4/site.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	
+	<title>Project traning</title>
+</head>
+<body>
+	<div id="wrapper">
+		<h2 class="text-center">Project trainning - xay dung website ban hang</h2>
+		<div class="navbar navbar-default navbar-static-top" role=navigation>
+			<div class="container">
+				<dir class="navbar-header">
+					<a href="/lab4/list_product.php" class="navbar-brand">danh sach san pham</a>
+					<a href="/lab4/add_product.php" class="navbar-brand">them san pham</a>
+				</dir>
+			</div>
+		</div>
